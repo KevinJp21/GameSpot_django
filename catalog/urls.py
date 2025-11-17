be99b49467a2751cc9245455ba1a55485e6cefbb
KevinJp21 <kevinjp821@gmail.com>
2025-11-17 14:39:01 -0500

    create platform_list

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('new_platform/', views.new_platform, name='new_platform'),
+    path('platform_list/', views.platform_list, name='platform_list'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Juego
+from django.shortcuts import render, redirect
+from .models import Juego, Plataforma
 from .forms import PlataformaForm
 
 # Create your views here.
@@ -11,13 +11,17 @@ def index(request):
 
 def new_platform(request):
     if request.method == 'POST':
-        form = PlataformaForm(request.POST)
-        if form.is_valid():
-            form.save()
+        platform_form = PlataformaForm(request.POST)
+        if platform_form.is_valid():
+            platform_form.save()
+            return redirect('platform_list')
     else:
-        form = PlataformaForm()
+        platform_form = PlataformaForm()
 
-    context = {
-        'form': form
-    }
-    return render(request, 'new_platform.html', context)
+    return render(request, 'new_platform.html', {'platform_form': platform_form})
+
+def platform_list(request):
+    platforms = Plataforma.objects.all()
+    return render(request, 'platform_list.html', {
+        'platforms': platforms
+    })
