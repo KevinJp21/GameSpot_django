bd5ab3f791015182d616b17ee53b0267de843ba0
KevinJp21 <kevinjp821@gmail.com>
2025-11-17 09:20:55 -0500

    add catalog models and migrations

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+class Plataforma(models.Model):
+    nombre = models.CharField(max_length=100)
+    fabricante = models.CharField(max_length=100, null=True, blank=True)
+    img_url = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Genero(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Desarrollador(models.Model):
+    nombre = models.CharField(max_length=150)
+    pais = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Juego(models.Model):
+    titulo = models.CharField(max_length=150)
+    descripcion = models.TextField(null=True, blank=True)
+    fecha_lanzamiento = models.DateField(null=True, blank=True)
+    plataforma = models.ForeignKey(Plataforma, on_delete=models.CASCADE)
+    genero = models.ForeignKey(Genero, on_delete=models.CASCADE)
+    desarrollador = models.ForeignKey(Desarrollador, on_delete=models.CASCADE)
+    img_url = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.titulo
