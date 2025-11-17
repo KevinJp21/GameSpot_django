a3bab8069e29185815204615a3d66627a307bf1a
KevinJp21 <kevinjp821@gmail.com>
2025-11-17 10:31:35 -0500

    include catalog urls

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Juego
 # Create your views here.
+def index(request):
+    juegos = Juego.objects.all()
+    return render(request, 'index.html', {
+        'juegos': juegos
+    })
